0fbcb84007fdcbf4ac538d517d705278df21d697
Kasia Piatek <piatekkatarzynaolga@gmail.com>
2017-12-03 15:51:32 +0100

    style.js

---
+++ style.js
@@ -0,0 +1,20 @@
+
+	var a = 100;
+	var b = -1;
+
+   	var value = (a * a) - (2 * a * b) + (b * b);
+   	console.log(value);
+
+   	if(value>0){
+   		console.log("wynik dodatni");
+   	}
+
+   	else if(value==0){
+   		console.log("równa się zero")
+   	}
+
+   	else {
+   		console.log("wynik ujemny");
+   	}
+
+
